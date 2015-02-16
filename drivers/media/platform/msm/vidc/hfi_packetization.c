84ebcd912dfb446a558eab88571c173bc3cc4df9
zephyr_2k <ankitnick1024@gmail.com>
2015-02-16 11:02:22 +0530

    msm: vidc: Add support for MBI extradata & Also fix build error

--- drivers/media/platform/msm/vidc/hfi_packetization.c
+++ drivers/media/platform/msm/vidc/hfi_packetization.c
@@ -357,6 +357,9 @@ static int get_hfi_extradata_index(enum hal_extradata_id index)
 	case HAL_EXTRADATA_STREAM_USERDATA:
 		ret = HFI_PROPERTY_PARAM_VDEC_STREAM_USERDATA_EXTRADATA;
 		break;
+	case HAL_EXTRADATA_METADATA_MBI:
+ 		ret = HFI_PROPERTY_PARAM_VENC_MBI_DUMPING;
+ 		break;
 	default:
 		dprintk(VIDC_WARN, "Extradata index not found: %d\n", index);
 		break;
